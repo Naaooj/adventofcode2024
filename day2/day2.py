1bae8537a8d63e17d704707bf67cfd01326fa2d3
Johann BERNEZ <jbernez@pictet-technologies.com>
2024-12-02 09:05:14 +0100

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,15 +1,24 @@
 import os
 
 def is_valid_sequence(sequence):
-    if len(sequence) < 2:
-        return False
+    def check_sequence(seq):
+        if len(seq) < 2:
+            return False
+        increasing = all(seq[i] < seq[i + 1] and 1 <= seq[i + 1] - seq[i] <= 3 for i in range(len(seq) - 1))
+        decreasing = all(seq[i] > seq[i + 1] and 1 <= seq[i] - seq[i + 1] <= 3 for i in range(len(seq) - 1))
+        return increasing or decreasing
 
-    increasing = all(sequence[i] < sequence[i + 1] and 1 <= sequence[i + 1] - sequence[i] <= 3 for i in range(len(sequence) - 1))
-    decreasing = all(sequence[i] > sequence[i + 1] and 1 <= sequence[i] - sequence[i + 1] <= 3 for i in range(len(sequence) - 1))
+    # Check if the sequence is valid as in part 1
+    if check_sequence(sequence):
+        return True
 
-    print(increasing, decreasing)
+    # Check if the sequence is valid as in part 2
+    for i in range(len(sequence)):
+        # Remove the i-th element from the sequence and check if the new sequence is valid
+        if check_sequence(sequence[:i] + sequence[i+1:]):
+            return True
 
-    return increasing or decreasing
+    return False
 
 def filter_sequences(input_file):
     with open(input_file, 'r') as infile:
